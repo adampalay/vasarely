46463a50e7a92b07eed369ebe817bf25eaa4a2fb
Adam Palay <adampalay@gmail.com>
2015-02-16 00:27:16 -0800

    refactor

--- generate.js
+++ generate.js
@@ -1,7 +1,7 @@
 $(function () {
 
     gridRows = 6;
-    gridColumns = 8;
+    gridColumns = 6;
 
     squareSize = 100;
 
@@ -15,13 +15,13 @@ $(function () {
 
     var randomChoice = function (arr) {
         index = Math.floor(Math.random() * arr.length);
-        return arr[index]
+        return arr[index];
     }
 
-    var getNextOuterColor = function (colors, lastColor, upColor) {
+    var getNextColor = function (colors, not1, not2) {
         color = randomChoice(colors);
-        if (color === lastColor || color === upColor) {
-            return getNextOuterColor(colors, lastColor, upColor);
+        if (color === not1 || color === not2) {
+            return getNextColor(colors, not1, not2);
         } else {
             return color;
         }
@@ -38,53 +38,38 @@ $(function () {
             if (i >= gridColumns) {
                 upColor = outerColors[i - gridColumns];
             };
-            nextOuterColor = getNextOuterColor(colors, lastColor, upColor);
+            nextOuterColor = getNextColor(colors, lastColor, upColor);
             outerColors[outerColors.length] = nextOuterColor;
         };
         return outerColors;
-    }
-
-    var generateColorArray = function (rowWidth, totalSquares) {
-        colorArray = [];
-        lastColor = null;
-        upColor = null;
-        for (var i = 0; i < totalSquares; i++) {
-            color = chooseFromArray(colors, lastColor);
-            lastColor = color;
-            if (i >= rowWidth - 1) {
-                upColor = colorArray[i - rowWidth + 1]
-            }
-
-            colorArray.push(color);
-        };
-        return colorArray
-    }
-
-    var chooseFromArray = function (arr, except) {
-        index = Math.floor(Math.random() * arr.length);
-        if (arr[index] === except) {
-            return chooseFromArray(arr, except);
-        }
-        return arr[index];
     };
 
-    var createSquare = function(index, colorArray) {
+    var generateInnerColors = function (outerColors) {
+        innerColors = [];
+        for (var i = 0; i < outerColors.length; i++) {
+            color = getNextColor(outerColors, outerColors[i], null)
+            innerColors[innerColors.length] = color;
+        };
+        return innerColors;
+    }
+
+    var createSquare = function(index, outerColors, innerColors) {
         cell = $("<div class='cell'/>");
         svg = $("<svg/>");
         outerSquare = $("<rect width='100' height='100' class='outer'/>")
-        outerColor = colorArray[index];
+        outerColor = outerColors[index];
         outerSquare.attr('fill', outerColor);
         svg.append(outerSquare);
 
-        shape = chooseFromArray(kinds, null);
+        shape = randomChoice(kinds);
         if (shape === 'circle') {
             innerShape = $('<circle cx="50" cy="50" r="35" class="inner"/>');
         } else {
             // it's a square
-                innerShape = $('<rect width="70" height="70" x="15" y="15" class="inner" />');
+            innerShape = $('<rect width="70" height="70" x="15" y="15" class="inner" />');
 
         };
-        innerShape.attr('fill', chooseFromArray(colors, outerColor));
+        innerShape.attr('fill', innerColors[index]);
         svg.append(innerShape);
         cell.append(svg);
         return cell
@@ -92,10 +77,10 @@ $(function () {
 
     var generate = function() {
         $("#grid").css("max-width", maxWidth);
-        colorArray = generateColorArray(gridColumns, totalSquares);
+        outerColors = generateOuterColors(colors, totalSquares, gridColumns);
+        innerColors = generateInnerColors(outerColors);
         for (var i = 0; i < totalSquares; i++) {
-
-            cell = createSquare(i, colorArray);
+            cell = createSquare(i, outerColors, innerColors);
             $("#grid").append(cell);
             // hack: http://stackoverflow.com/questions/3642035/jquerys-append-not-working-with-svg-element
             $("#grid").html($("#grid").html());
