043ac17a5d380fa62c2e3cd0cb5d729886414d85
Adam Palay <adampalay@gmail.com>
2015-02-16 17:33:12 -0800

    clean up

--- generate.js
+++ generate.js
@@ -1,25 +1,19 @@
 $(function () {
 
-    gridRows = 6;
-    gridColumns = 6;
-
-    squareSize = 100;
-
-    maxWidth = squareSize * gridColumns;
-
-    totalSquares = gridRows * gridColumns;
-
-    colors = ['red', 'yellow', 'purple', 'orange', 'green', 'blue', 'violet', 'cyan'];
-    kinds = ['circle', 'square'];
-
+    var gridRows = 6,
+        gridColumns = 6,
+        squareSize = 100,
+        maxWidth = squareSize * gridColumns,
+        colors = ['red', 'yellow', 'purple', 'orange', 'green', 'blue', 'violet', 'cyan'],
+        kinds = ['circle', 'square'];
 
     var randomChoice = function (arr) {
-        index = Math.floor(Math.random() * arr.length);
+        var index = Math.floor(Math.random() * arr.length);
         return arr[index];
     }
 
     var getNextColor = function (colors, not1, not2) {
-        color = randomChoice(colors);
+        var color = randomChoice(colors);
         if (color === not1 || color === not2) {
             return getNextColor(colors, not1, not2);
         } else {
@@ -28,9 +22,10 @@ $(function () {
     }
 
     var generateOuterColors = function (colors, totalSquares, gridColumns) {
-        outerColors = [];
-        lastColor = null;
-        upColor = null;
+        var outerColors = [],
+            lastColor = null,
+            upColor = null,
+            nextOuterColor;
         for (var i = 0; i < totalSquares; i++) {
             if (i > 0) {
                 lastColor = outerColors[outerColors.length - 1];
@@ -45,7 +40,8 @@ $(function () {
     };
 
     var generateInnerColors = function (outerColors) {
-        innerColors = [];
+        var innerColors = [],
+            color;
         for (var i = 0; i < outerColors.length; i++) {
             color = getNextColor(outerColors, outerColors[i], null)
             innerColors[innerColors.length] = color;
@@ -54,14 +50,16 @@ $(function () {
     }
 
     var createSquare = function(index, outerColors, innerColors) {
-        cell = $("<div class='cell'/>");
-        svg = $("<svg/>");
-        outerSquare = $("<rect width='100' height='100' class='outer'/>")
-        outerColor = outerColors[index];
+        var cell = $("<div class='cell'/>"),
+            svg = $("<svg/>"),
+            outerSquare = $("<rect width='100' height='100' class='outer'/>"),
+            outerColor = outerColors[index],
+            shape = randomChoice(kinds),
+            innerShape;
+
         outerSquare.attr('fill', outerColor);
         svg.append(outerSquare);
 
-        shape = randomChoice(kinds);
         if (shape === 'circle') {
             innerShape = $('<circle cx="50" cy="50" r="35" class="inner"/>');
         } else {
@@ -76,14 +74,16 @@ $(function () {
     };
 
     var generate = function(colors, gridRows, gridColumns) {
-        maxWidth = squareSize * gridColumns;
+        var maxWidth = squareSize * gridColumns,
+            totalSquares = gridRows * gridColumns,
+            outerColors = generateOuterColors(colors, totalSquares, gridColumns),
+            innerColors = generateInnerColors(outerColors),
+            cell;
+
         $("#grid").empty();
         $("#grid").css("max-width", maxWidth);
         $('input[value="rows"]').val(gridRows);
         $('input[value="columns"]').val(gridColumns);
-        totalSquares = gridRows * gridColumns;
-        outerColors = generateOuterColors(colors, totalSquares, gridColumns);
-        innerColors = generateInnerColors(outerColors);
         for (var i = 0; i < totalSquares; i++) {
             cell = createSquare(i, outerColors, innerColors);
             $("#grid").append(cell);
@@ -105,18 +105,19 @@ $(function () {
 
     $("#generate").on("click", function(event){
         event.preventDefault();
-        var colors = []
-        var colorCheckBoxes = $("input[name=color]");
+        var colors = [],
+            colorCheckBoxes = $("input[name=color]"),
+            rawRows = $('input[value="rows"]').val(),
+            rawColumns = $('input[value="columns"]').val(),
+            gridRows = parseInt(rawRows),
+            gridColumns = parseInt(rawColumns);
+
+
         for (var i = 0; i < colorCheckBoxes.length; i++) {
             if (colorCheckBoxes[i].checked) {
                 colors[colors.length] = colorCheckBoxes[i].value;
             };
         };
-        rawRows = $('input[value="rows"]').val();
-        rawColumns = $('input[value="columns"]').val();
-
-        gridRows = parseInt(rawRows);
-        gridColumns = parseInt(rawColumns);
 
         if (gridRows.toString() === "NaN" || gridColumns.toString() === "NaN") {
             alert("Please enter integer amounts for rows and columns")
